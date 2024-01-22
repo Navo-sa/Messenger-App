0b50f77dd28670bd361dc681eea8218a468dfceb
shanaka <shanakairesh003@gmail.com>
2024-01-22 20:39:25 +0530

    addEventlistners

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,26 @@ const room = document.querySelectorAll('.but1')
 const memButton = document.querySelectorAll('.memberbutton')
 const display = document.querySelector('.messgebody')
 const inptMsg = document.querySelector('.sendmessage')
+
+room.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault()
+        const inptRoom = element.innerText
+        console.log(inptRoom)
+    })
+})
+
+memButton.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault()
+        const inptMem = element.innerText
+        console.log(inptMem)
+    })
+})
+
+inptMsg.addEventListener('click', (e) => {
+    e.preventDefault()
+    const inptMessage = inptMsg.inputmsg.value
+    console.log(inptMessage)
+})
+
