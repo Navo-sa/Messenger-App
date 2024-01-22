2350211989c4b4a76e7311b1c2d3e2b3c04fa6c5
shanaka <shanakairesh003@gmail.com>
2024-01-22 21:35:34 +0530

    template

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
-
-
+import { chat } from './chat.js'
+import { ui } from './ui.js'
 
 
 const room = document.querySelectorAll('.but1')
@@ -7,11 +7,18 @@ const memButton = document.querySelectorAll('.memberbutton')
 const display = document.querySelector('.messgebody')
 const inptMsg = document.querySelector('.sendmessage')
 
+const newChat = new chat('Charitha', 'Shanaka', 'General')
+const newUi = new ui(display)
+
+
 room.forEach((element) => {
     element.addEventListener('click', (e) => {
         e.preventDefault()
         const inptRoom = element.innerText
-        console.log(inptRoom)
+        newChat.updateRoom(inptRoom)
+        getChatRoom((snapshot) => {
+            newUi.render(snapshot)
+        })
     })
 })
 
@@ -19,13 +26,16 @@ memButton.forEach((element) => {
     element.addEventListener('click', (e) => {
         e.preventDefault()
         const inptMem = element.innerText
-        console.log(inptMem)
+        newChat.updateMemName(inptMem)
+        getChatMem((snapshot) => {
+            newUi.render(snapshot)
+        })
     })
 })
 
 inptMsg.addEventListener('click', (e) => {
     e.preventDefault()
     const inptMessage = inptMsg.inputmsg.value
-    console.log(inptMessage)
+    newChat.addChat(inptMessage)
 })
 
