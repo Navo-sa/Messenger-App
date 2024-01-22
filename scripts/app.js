bb78a88d035cd6b596c101c6716e5c8cfe938c1f
shanaka <shanakairesh003@gmail.com>
2024-01-22 20:19:28 +0530

    hooking all element

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+
+
+
+
+const room = document.querySelectorAll('.but1')
+const memButton = document.querySelectorAll('.memberbutton')
+const display = document.querySelector('.messgebody')
+const inptMsg = document.querySelector('.sendmessage')
